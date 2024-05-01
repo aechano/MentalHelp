3000445e9aaf20de8768042ac9e662ecb3721a7f
MurataUgetsu <yanezakai@gmail.com>
2024-05-01 18:35:24 +0800

    Light mode

--- app/src/main/java/com/example/mentalhelp/Application/MentalHelp.java
+++ app/src/main/java/com/example/mentalhelp/Application/MentalHelp.java
@@ -3,11 +3,14 @@ package com.example.mentalhelp.Application;
 import android.app.Application;
 import android.media.MediaPlayer;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class MentalHelp extends Application {
     MediaPlayer mediaPlayer;
     @Override
     public void onCreate() {
         super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
     @Override
